33b80ae3c3bd32f56852a6fc17b474f942e0460d
Henry Chong <henrychong@Henrys-MacBook-Pro-2.local>
2021-11-15 19:29:37 -0500

    navigation updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import Home from '../src/Home';
-import Navbar from "./components/Navbar";
+import React from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Home from './screens/Home';
+import About from './screens/About';
+import Contact from './screens/Contact';
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Home />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/contact' element={<Contact />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import logo from '../logo.svg';
-import Navbar from "../components/Navbar";
 import '../App.css';
 
 export default function Home() {
