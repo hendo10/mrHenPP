9b37b8efad38e246a24be6e80cb5ca9f3f71397a
Henry Chong <henrychong@Henrys-MacBook-Pro-2.local>
2021-11-23 12:30:58 -0500

    update social icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,60 +5,73 @@ import slidePdf from '../assets/cv/hchongCV112021vSlides.pdf';
 import wordPdf from '../assets/cv/hchongCV112012vDoc.pdf';
 import styled, { css } from 'styled-components';
 
+const StyledResumeLink = styled.div`
+    .resume-button {
+        padding: 10px 22px 10px 22px;
+        font-size: 16px;
+        height: 45px;
+        text-decoration: none;
+    }
+
+    a {
+        color: #2C8DE7;
+    }
+
+    a:visited {
+        color: #2C8DE7;
+    }
+
+    a:hover, a:active, a:focus {
+        color: #2C8DE7;
+    }
+`;
+
 export default function Navbar() {
     const navigateToPath = useNavigate();
 
-    const StyledResumeLink = styled.div`
-        .resume-button {
-            margin-left: 15px;
-            font-size: var(--fz-xs);
-        }
-    `;
+    const ResumeLink = (
+        <a className="resume-button" href={slidePdf} target="_blank">Resume</a>
+    );
 
     return (
         <View style={styles.container}>
             <View style={{width: '76%', flexDirection: 'row', alignItems: 'center'}}>
-            <View style={{flex: 1, alignItems:'flex-start'}}>
-                <TouchableOpacity 
-                    onPress={() => navigateToPath('/')}
-                    style={{paddingRight: 15}}
-                >
-                    <Text style={styles.mainText}>Home</Text>
-                </TouchableOpacity>
-            </View>
-            <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end',}}>
-                <TouchableOpacity 
-                    onPress={() => navigateToPath('/about')}
-                    style={{paddingRight: 15}}
-                >
-                    <Text style={styles.mainText}>About</Text>
-                </TouchableOpacity>
-                <TouchableOpacity 
-                    onPress={() => navigateToPath('/projects')}
-                    style={{paddingRight: 15}}
-                >
-                    <Text style={styles.mainText}>Projects</Text>
-                </TouchableOpacity>
-                <TouchableOpacity 
-                    onPress={() => navigateToPath('/contact')}
-                    style={{paddingRight: 15}}
-                >
-                    <Text style={styles.mainText}>Contact</Text>
-                </TouchableOpacity>
-                <TouchableOpacity 
-                    onPress={() => {}}
-                    style={styles.resumeLinkContainer}
-                >
-                    <View style={{paddingVertical: 10, paddingHorizontal: 22}}>
-                        <a style='' href={slidePdf} target="_blank">
-                            Resume
-                        </a>
-                        {/* <a href={wordPdf} target="_blank">
-                            Resume
-                        </a> */}
-                    </View>  
-                </TouchableOpacity>
-            </View>
+                <View style={{flex: 1, alignItems:'flex-start'}}>
+                    <TouchableOpacity 
+                        onPress={() => navigateToPath('/')}
+                        style={{paddingRight: 15}}
+                    >
+                        <Text style={styles.mainText}>Home</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end',}}>
+                    <TouchableOpacity 
+                        onPress={() => navigateToPath('/about')}
+                        style={{paddingRight: 15}}
+                    >
+                        <Text style={styles.mainText}>About</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity 
+                        onPress={() => navigateToPath('/projects')}
+                        style={{paddingRight: 15}}
+                    >
+                        <Text style={styles.mainText}>Projects</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity 
+                        onPress={() => navigateToPath('/contact')}
+                        style={{paddingRight: 15}}
+                    >
+                        <Text style={styles.mainText}>Contact</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity 
+                        onPress={() => {}}
+                        style={styles.resumeLinkContainer}
+                    >
+                        <StyledResumeLink>
+                            {ResumeLink}
+                        </StyledResumeLink>
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>  
     )
@@ -84,5 +97,7 @@ const styles = StyleSheet.create({
         borderWidth: 1.5, 
         borderStyle: 'dashed', 
         borderRadius: 10,
+        height: 40,
+        justifyContent: 'center'
     }
 })
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,79 +4,63 @@ import Social from '../components/Social';
 
 export default function Home() {
     return (
-        <View style={[styles.mainSection]}>
-            
-            <View style={[styles.mainTextContainer]}>
-                <Text style={[styles.mainText, {marginVertical: 25}]}>Hi, my name is</Text>
-                <Text style={styles.mainTitle}>
-                    Henry Chong.
-                </Text>
-                <Text style={styles.secondaryTitle}>
-                    I am passionate about creative solutions and {'\n'}the role technology plays in our lives.
-                </Text>
-                <Text style={[styles.mainText, {marginVertical: 25}]}>
-                    I'm a software engineer with a background in finance. {'\n'}
-                    Engaged in problem solving and building out solutions, {'\n'}I enjoy creating clean user interfaces and front end implementation of applications.
-                </Text>
-            </View>
-            <View style={styles.profileContainer}>
-                {/* profile picture to be placed here */}
-            </View>
-            <View style={{}}>
-                <Social />
+        <View style={[styles.container]}>
+            <View style={[styles.bodyContainer]}>
+                <View style={{width: '76%'}}>
+                    <Text style={styles.mainText}>Hi, my name is</Text>
+                    <Text style={[styles.mainTitle, {paddingVertical: 15}]}>Henry Chong.</Text>
+                    <Text style={styles.secondaryTitle}>
+                        I am passionate about creative solutions and {'\n'}the role technology plays in our lives.
+                    </Text>
+                    <Text style={[styles.mainText, {marginVertical: 15}]}>
+                        I'm a software engineer with a background in finance. {'\n'}
+                        Engaged in problem solving and building out solutions, {'\n'}I enjoy creating clean user interfaces and front end implementation of applications.
+                    </Text>
+                </View>
+                <View style={styles.profileContainer}>
+                    {/* profile picture to be placed here */}
+                </View>
+                <View style={{width: '76%'}}>
+                    <Social />
+                </View>
             </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-//   footer: {
-//       width: "100%", 
-//       backgroundColor: "#151515", 
-//       flexDirection: "column", 
-//       paddingVertical: 50, 
-//       flexWrap: "wrap"
-//   },
-//   footerLinksContainer: {
-//       width: "100%", 
-//       flexDirection: "row", 
-//       marginTop: 40, 
-//       flexWrap: "wrap"
-//   },
-    profileContainer: {
-        flex: 1, 
-        minWidth: 320
-    },
-    mainSection: {
+    container: {
         flex: 1,
         paddingVertical: 25, 
         backgroundColor: 'white',
         flexDirection: 'column',
     },
+    bodyContainer: {
+        flex: 1, 
+        alignItems: "center", 
+        justifyContent: "center",  
+        marginTop: 50, 
+        marginBottom: 25,
+    },
+    profileContainer: {
+        flex: 1, 
+        // minWidth: 320
+    },
     mainTitle: {
         fontWeight: "800", 
-        fontSize: 52, 
-        width: '76%', 
+        fontSize: 52,
         color: "#262626"
     },
     secondaryTitle: {
         fontWeight: "600", 
         fontSize: 33, 
-        width: '76%', 
         color: "#262626",
         opacity: 0.65
     },
     mainText: {
         fontSize: 19, 
         opacity: 0.65, 
-        width: '76%',
         lineHeight: 30,
     },
-    mainTextContainer: {
-        flex: 1, 
-        alignItems: "center", 
-        justifyContent: "center",  
-        marginTop: 50, 
-        marginBottom: 25,
-    }
+
 });
